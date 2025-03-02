7dcf1213d57d2a57307e836a93307e928ae92304
Mueen Arif <muhammadmueenarif786@gmail.com>
2025-03-02 14:01:28 +0500

    header basic structure add. remove unnecessary code

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Header from "@/components/Header/page"
+
+export default function Home() {
+  return (
+    <>
+    <Header/>
+    <h1>Ecommerce Project</h1>
+    </>
+  );
+}
---
+++ components/Header/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <div>
+        <div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header;
